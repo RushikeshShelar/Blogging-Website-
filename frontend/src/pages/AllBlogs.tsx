42120feba2cc35aa63b058d10686a9e9d9c55a1d
Rushi <rushikeshshelar.cs@gmail.com>
2024-04-20 01:03:30 +0530

    All Blogs page and Individual Blog page

--- frontend/src/pages/AllBlogs.tsx
+++ frontend/src/pages/AllBlogs.tsx
@@ -1,9 +1,38 @@
+import AppBar from "../components/AppBar";
+import { BlogCard } from "../components/BlogCard";
+import { useBlogs } from "../hooks";
+
 const AllBlogs = () => {
-    return ( 
+
+    const { loading, blogs } = useBlogs();
+
+    if(loading) {
+        return (
+            <div>
+                Loading....
+            </div>
+        )
+    }
+
+    return (
         <div>
-            All Blogs
+            <AppBar />
+            <div className="flex justify-center">
+                <div className="max-w-xl">
+                    {blogs.map((blog,idx) => (
+                        <BlogCard
+                        key={idx}
+                        authorName={blog.author.name || "Anonymous"} 
+                        title={blog.title} 
+                        content={blog.content} 
+                        id={blog.id}
+                        publishedDate="December 3, 2023"
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default AllBlogs;
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,9 +1,29 @@
+import { useParams } from "react-router-dom";
+import FullBlog from "./FullBlog";
+import { useBlog } from "../hooks";
+
 const Blog = () => {
-    return ( 
+    const { id } = useParams()
+
+    const { loading, post } = useBlog({ id: Number(id || "1") });
+
+    if (loading) {
+        return (
+            <div>
+                Loading....
+            </div>
+        )
+    }
+
+    if (!post) {
+        return null;
+    }
+
+    return (
         <div>
-            Blog
+            <FullBlog blog={post} />
         </div>
-     );
+    );
 }
- 
+
 export default Blog;
