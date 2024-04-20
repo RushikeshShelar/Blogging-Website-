f04095388b30e31a551823e40ac40fd525da4b43
Rushi <rushikeshshelar.cs@gmail.com>
2024-04-20 17:15:49 +0530

    Coded landing Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Blog, SignIn, SignUp } from './pages'
 import AllBlogs from './pages/AllBlogs'
 import PublishBlog from './pages/PublishBlog'
+import LandingPage from './pages/Landingpage'
 
 const router = [
+  {
+    path: "/",
+    element: <LandingPage />
+  },
   {
     path: "/signin",
     element: <SignIn />
